2398aa359c85aacf128a076f264c4b3a0f1bdb88
小峰 <160499844@qq.com>
2021-01-12 22:58:52 +0800

    增加mp4切割成ts工具类

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,5 @@
 import os
-
+import shutil
 
 #图片常用格式
 img_endWiths = ['.jpg', '.png', '.gif', 'jpeg', 'bmp', 'webp', 'webp', 'tif']
@@ -54,10 +54,45 @@ def findAllFileVideo(base):
     return list
 
 
+def mkdir(path):
+    # 引入模块
+    import os
+
+    # 去除首位空格
+    path = path.strip()
+    # 去除尾部 \ 符号
+    path = path.rstrip("\\")
+
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists = os.path.exists(path)
+
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
+        os.makedirs(path)
+
+        print(path + ' 创建成功')
+
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print(path + ' 目录已存在')
+        return False
+
+
+
 def mp4ToM3u8(src_path, new_path, name):
 
     ts_name = os.path.splitext(name)[0] + '.ts'
     m3u8_name = os.path.splitext(name)[0] + '.m3u8'
+
+    # 定义要创建的目录
+    new_path = new_path + os.path.splitext(name)[0] + '/'
+    # 调用函数
+    mkdir(new_path)
 
 
     cmd = "ffmpeg -y -i \"%s\" -vcodec copy -acodec copy -vbsf h264_mp4toannexb \"%s\"" %(src_path + name, new_path + ts_name)
@@ -77,7 +112,10 @@ def mp4ToM3u8(src_path, new_path, name):
     print(d)
 
 
+    #删除ts文件
+    os.remove(new_path + ts_name)
+
 if __name__ == '__main__':
     #print(getResponseHeader("JPG"))
 
-    mp4ToM3u8('X:/yy直播/','D:/web_application/nginx 1.7.11.3 Gryphon/html/','15012_1101280606_54880976_1598567424896.mp4')
+    mp4ToM3u8('X:/yy直播/','D:/web_application/data/movies/','15012_1101280606_54880976_1598571903758.mp4')
